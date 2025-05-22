1d6922e448c345ec2448793e93b62bce4086d611
zarif-webflow <zarif.webflow@gmail.com>
2025-05-22 22:31:43 +0600

    refactor: move the combobox logic to its own file

--- dist/display-property-search.js
+++ dist/display-property-search.js
@@ -1,45 +1,62 @@
+const SELECTORS$1 = {
+  comboboxInput: "[data-input-combobox=input]",
+  comboboxWrapper: "[data-input-combobox=wrapper]",
+  resultContainer: "[data-input-combobox=result-container]",
+  resultList: "[data-input-combobox=result-list]",
+  resultItem: "[data-input-combobox=result-item]",
+  resultItemValue: "[data-input-combobox=result-item-value]"
+};
+const setupCombobox = (comboboxInput) => {
+  const id = comboboxInput.id;
+  if (!id) {
+    console.error("Combobox input does not have an id", comboboxInput);
+    return;
+  }
+  const resultContainer = comboboxInput.closest(SELECTORS$1.comboboxWrapper)?.querySelector(SELECTORS$1.resultContainer);
+  if (!resultContainer) {
+    console.log("Result container wasnt found for", comboboxInput);
+    return;
+  }
+  const resultList = resultContainer.querySelector(SELECTORS$1.resultList);
+  if (!resultList) {
+    console.log("Result list wasnt found for", comboboxInput);
+    return;
+  }
+  const resultItem = resultList.querySelector(SELECTORS$1.resultItem);
+  if (!resultItem) {
+    console.log("Result item wasnt found for", comboboxInput);
+    return;
+  }
+  const modalFragment = document.createDocumentFragment();
+  const optionListId = id + "-listbox";
+  modalFragment.appendChild(resultContainer);
+  resultContainer.dataset.initialized = "true";
+  const setupInitialAttributes = () => {
+    comboboxInput.type = "search";
+    comboboxInput.ariaAutoComplete = "both";
+    comboboxInput.setAttribute("autocomplete", "off");
+    comboboxInput.setAttribute("autocorrect", "off");
+    comboboxInput.setAttribute("autocapitalize", "off");
+    comboboxInput.setAttribute("spellcheck", "false");
+    comboboxInput.setAttribute("aria-controls", optionListId);
+    resultList.role = "listbox";
+    resultList.id = optionListId;
+  };
+  setupInitialAttributes();
+};
+
 const SELECTORS = {
-  searchInput: "[data-display-property-element=search-input]",
-  searchWrapper: "[data-display-property-element=search-wrapper]",
-  searchResultContainer: "[data-display-property-element=search-result-cont]",
-  searchResultList: "[data-display-property-element=search-result-list]",
-  searchResultItem: "[data-display-property-element=search-result-item]",
-  searchResultItemValue: "[data-display-property-element=search-result-item-value]"
+  comboboxInput: "[data-input-combobox=input]",
+  comboboxWrapper: "[data-input-combobox=wrapper]",
+  resultContainer: "[data-input-combobox=result-container]",
+  resultList: "[data-input-combobox=result-list]",
+  resultItem: "[data-input-combobox=result-item]",
+  resultItemValue: "[data-input-combobox=result-item-value]"
 };
 const init = () => {
-  const searchInputs = Array.from(document.querySelectorAll(SELECTORS.searchInput));
-  for (const searchInput of searchInputs) {
-    const resultContainer = searchInput.closest(SELECTORS.searchWrapper)?.querySelector(SELECTORS.searchResultContainer);
-    if (!resultContainer) {
-      console.log("Result container wasnt found for", searchInput);
-      continue;
-    }
-    const resultList = resultContainer.querySelector(SELECTORS.searchResultList);
-    if (!resultList) {
-      console.log("Result list wasnt found for", searchInput);
-      continue;
-    }
-    const resultItem = resultList.querySelector(SELECTORS.searchResultItem);
-    if (!resultItem) {
-      console.log("Result item wasnt found for", searchInput);
-      continue;
-    }
-    const modalFragment = document.createDocumentFragment();
-    const optionListId = resultList.id || "prop-search";
-    modalFragment.appendChild(resultContainer);
-    resultContainer.dataset.initialized = "true";
-    const setupInitialAttributes = () => {
-      searchInput.type = "search";
-      searchInput.ariaAutoComplete = "both";
-      searchInput.setAttribute("autocomplete", "off");
-      searchInput.setAttribute("autocorrect", "off");
-      searchInput.setAttribute("autocapitalize", "off");
-      searchInput.setAttribute("spellcheck", "false");
-      searchInput.setAttribute("aria-controls", optionListId);
-      resultList.role = "listbox";
-      resultList.id = optionListId;
-    };
-    setupInitialAttributes();
+  const comboboxInputs = Array.from(document.querySelectorAll(SELECTORS.comboboxInput));
+  for (const input of comboboxInputs) {
+    setupCombobox(input);
   }
 };
 init();
