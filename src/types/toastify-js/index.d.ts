384126eab8d6fb5d08d54caebbddecb39b574ecb
zarif-webflow <zarif.webflow@gmail.com>
2025-05-24 02:41:12 +0600

    feat: add toastify js with types

---
+++ src/types/toastify-js/index.d.ts
@@ -0,0 +1,49 @@
+declare module 'toastify-js' {
+  export interface ToastifyOffset {
+    x: number | string;
+    y: number | string;
+  }
+
+  export interface ToastifyOptions {
+    oldestFirst?: boolean;
+    text?: string;
+    node?: Node;
+    duration?: number;
+    selector?: string | HTMLElement | ShadowRoot;
+    callback?: () => void;
+    destination?: string;
+    newWindow?: boolean;
+    close?: boolean;
+    gravity?: string;
+    positionLeft?: boolean;
+    position?: string;
+    backgroundColor?: string;
+    avatar?: string;
+    className?: string;
+    stopOnFocus?: boolean;
+    onClick?: () => void;
+    offset?: ToastifyOffset;
+    escapeMarkup?: boolean;
+    ariaLive?: string;
+    style?: { [property: string]: string };
+  }
+
+  export interface ToastifyInstance {
+    options: ToastifyOptions;
+    toastElement: HTMLElement | null;
+    toastify: string;
+    init(options?: ToastifyOptions): this;
+    buildToast(): HTMLElement;
+    showToast(): this;
+    hideToast(): void;
+    removeElement(toastElement: HTMLElement): void;
+  }
+
+  function Toastify(options?: ToastifyOptions): ToastifyInstance;
+  namespace Toastify {
+    const defaults: ToastifyOptions;
+    function reposition(): void;
+  }
+
+  export default Toastify;
+}
---
+++ src/utils/toast.ts
@@ -0,0 +1,34 @@
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+
+const toasterProps = {
+  close: true,
+  gravity: 'bottom', // `top` or `bottom`
+  position: 'right', // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  className: 'toast-style',
+};
+
+export type ToastProps = {
+  text: string;
+  duration?: number;
+  position?: string;
+  isTop?: boolean;
+  offsetX?: string;
+  offsetY?: string;
+};
+
+export const getToaster = ({ text, duration, position, isTop, offsetX, offsetY }: ToastProps) => {
+  const toaster = Toastify({
+    text,
+    duration: duration || 3000,
+    ...toasterProps,
+    position,
+    gravity: isTop ? 'top' : 'bottom',
+    offset: {
+      x: offsetX || '1em', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: offsetY || '1em', // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  });
+  return toaster;
+};
