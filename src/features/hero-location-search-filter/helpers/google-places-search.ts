f6f87661de160eb38f7d55d2775b5c2d0216ecfd
zarif-webflow <zarif.webflow@gmail.com>
2026-02-02 22:58:45 +0600

    chore: wrap google places search initializer into a function for better error handling

--- src/features/hero-location-search-filter/helpers/google-places-search.ts
+++ src/features/hero-location-search-filter/helpers/google-places-search.ts
@@ -1,69 +1,80 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { getActiveScript } from "@taj-wf/utils";
 
-const scriptElement = getActiveScript(import.meta.url);
-
-if (!scriptElement) {
-  throw new Error("Search Address script element was not found!");
-}
-
-const placesApiKey = scriptElement.getAttribute("data-places-key");
-
-if (!placesApiKey) {
-  throw new Error(
-    "Places API key was not found in the script element! Please set data-places-key attribute in the script."
-  );
-}
-
-const loader = new Loader({
-  apiKey: placesApiKey,
-  version: "weekly",
-});
-
 const formatResult = (location: string) => {
   return location.replaceAll(" - ", ", ").replaceAll(" | ", ", ");
 };
 
-const getGooglePlacesSearch = async () => {
-  const { AutocompleteSuggestion } = await loader.importLibrary("places");
+export const initGooglePlacesSearch = () => {
+  const scriptElement = getActiveScript(import.meta.url);
 
-  const fetchLocations = async (query: string): Promise<string[]> => {
-    try {
-      const request: google.maps.places.AutocompleteRequest = {
-        input: query,
-        includedRegionCodes: ["ae"],
-      };
+  if (!scriptElement) {
+    throw new Error("Search Address script element was not found!");
+  }
 
-      const response = await AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
+  const placesApiKey = scriptElement.getAttribute("data-places-key");
 
-      if (!response) {
-        console.error("No response from AutocompleteSuggestion");
+  if (!placesApiKey) {
+    throw new Error(
+      "Places API key was not found in the script element! Please set data-places-key attribute in the script."
+    );
+  }
+
+  const loader = new Loader({
+    apiKey: placesApiKey,
+    version: "weekly",
+  });
+
+  const getGooglePlacesSearch = async () => {
+    const { AutocompleteSuggestion } = await loader.importLibrary("places");
+
+    const search = async (query: string): Promise<string[]> => {
+      try {
+        const request: google.maps.places.AutocompleteRequest = {
+          input: query,
+          includedRegionCodes: ["ae"],
+          includedPrimaryTypes: [
+            "natural_feature",
+            "sublocality",
+            "apartment_complex",
+            "neighborhood",
+            "condominium_complex",
+          ],
+        };
+
+        const response = await AutocompleteSuggestion.fetchAutocompleteSuggestions(request);
+
+        if (!response) {
+          console.error("No response from AutocompleteSuggestion");
+          return [];
+        }
+
+        if (response.suggestions.length === 0) {
+          return [];
+        }
+
+        const results: string[] = [];
+
+        for (const suggestion of response.suggestions) {
+          const mainLocationResult = suggestion?.placePrediction?.mainText?.text;
+          if (!mainLocationResult) continue;
+
+          const formattedResult = formatResult(mainLocationResult);
+
+          results.push(formattedResult);
+        }
+
+        return results;
+      } catch (error) {
+        console.error("Something went wrong with places api:", error);
         return [];
       }
+    };
 
-      if (response.suggestions.length === 0) {
-        return [];
-      }
-
-      const results: string[] = [];
-
-      for (const suggestion of response.suggestions) {
-        const mainLocationResult = suggestion?.placePrediction?.mainText?.text;
-        if (!mainLocationResult) continue;
-
-        const formattedResult = formatResult(mainLocationResult);
-
-        results.push(formattedResult);
-      }
-
-      return results;
-    } catch (error) {
-      console.error("Something went wrong with places api:", error);
-      return [];
-    }
+    return { search };
   };
 
-  return { fetchLocations };
+  return { getGooglePlacesSearch };
 };
 
-export { getGooglePlacesSearch };
+export type GooglePlacesSearchFunc = (query: string) => Promise<string[]>;
